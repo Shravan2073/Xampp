d34a304d83f129c5f5a000d6085ba97c1240bf18
Shravan2073 <145554933+Shravan2073@users.noreply.github.com>
2024-04-12 12:25:24 +0530

    Create admin_dashboard.php
    Signed-off-by: Shravan2073 <145554933+Shravan2073@users.noreply.github.com>

---
+++ admin_dashboard.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["admin"])) {
+    header("Location: admin_login.html");
+    exit();
+}
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user_management";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "ID: " . $row["id"]. " - Username: " . $row["username"]. " - Email: " . $row["email"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
